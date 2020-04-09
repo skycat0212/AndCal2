938e5e55cafd02884df6e32536ef8b9cde4f7a11
KSJ <mr_magnet@naver.com>
2020-04-09 21:04:14 +0900

    리스너 하나로 수정중

--- AndCal2/app/src/main/java/com/sslab/andcal2/Calculator.java
+++ AndCal2/app/src/main/java/com/sslab/andcal2/Calculator.java
@@ -7,14 +7,17 @@ public class Calculator {
     private double price2;
     private double result;
 
-    private int dustkswk
+    private int operator; // 연산자 0 초기화, 1 은 연산이어서 할때, 2 + , 3 - , 4 * , 5 / // 이퀄 제외하기
     private int priceFlag; // 0 = price1, 1 = price2
+//    private int equalFlag;
 
     Calculator(){
         price1 = 0;
         price2 = 0;
         result = 0;
         priceFlag = 0;
+        operator = 0;
+//        equalFlag = 0;
     }
 
     public void setPriceFlag(int flag ){
@@ -32,37 +35,35 @@ public class Calculator {
     public void setResult(double inputPrice){ result = inputPrice; }
     public double getResult(){ return result; }
 
+    public void setOperator(int inputOperator){ operator = inputOperator;}
+    public int getOperator(){ return  operator;}
+
+//    public void setEqualFlag(int inputEqualFlag){ equalFlag = inputEqualFlag;}
+//    public int getEqualFlag(){ return  equalFlag;}
+
     public double divide(double inputPrice1, double inputPrice2){ return inputPrice1 / inputPrice2; }
     public double multiple(double inputPrice1, double inputPrice2){ return inputPrice1 * inputPrice2; }
     public double minus(double inputPrice1, double inputPrice2){ return inputPrice1 - inputPrice2; }
     public double plus(double inputPrice1, double inputPrice2){ return inputPrice1 + inputPrice2; }
 
-    public double equal(double inputPrice1){
-        return inputPrice1;
-    }
-    public double equal(double inputPrice1, int caseNum, double inputPrice2){
-        return doCase(inputPrice1,inputPrice2, caseNum);
-    }
 
-    public double doCase(double inputPrice1, double inputPrice2, int caseNum){
+    public double doCase(int operator){
         double result=0;
-        switch (caseNum){
-            case 1:
-                result = divide(inputPrice1,inputPrice2);
-                break;
-            case 2:
-                result = multiple(inputPrice1,inputPrice2);
-                break;
-            case 3:
-                result = minus(inputPrice1,inputPrice2);
+        switch (operator){
+            case 5:
+                result = divide(price1,price2);
                 break;
             case 4:
-                result = plus(inputPrice1,inputPrice2);
+                result = multiple(price1,price2);
+                break;
+            case 3:
+                result = minus(price1,price2);
+                break;
+            case 2:
+                result = plus(price1,price2);
                 break;
         }
         return result;
     }
-
-
 
 }
--- AndCal2/app/src/main/java/com/sslab/andcal2/MainActivity.java
+++ AndCal2/app/src/main/java/com/sslab/andcal2/MainActivity.java
@@ -3,36 +3,104 @@ package com.sslab.andcal2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Calculator calculator;
     private TextView textView;
+
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+    private Button zero;
+
+    private Button ac;
+    private Button equal;
+    private Button plus;
+    private Button minus;
+    private Button multiple;
+    private Button divide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calculator = new Calculator();
+
+        textView = (TextView)findViewById(R.id.result);
+        textView.setText("");
+
+        one = (Button)findViewById(R.id.bOne);
+        two = (Button)findViewById(R.id.bTwo);
+        three = (Button)findViewById(R.id.bThree);
+        four = (Button)findViewById(R.id.bFour);
+        five = (Button)findViewById(R.id.bFive);
+        six = (Button)findViewById(R.id.bSix);
+        seven = (Button)findViewById(R.id.bSeven);
+        eight = (Button)findViewById(R.id.bEight);
+        nine = (Button)findViewById(R.id.bNine);
+        zero = (Button)findViewById(R.id.bZero);
+
+
+        ac = (Button)findViewById(R.id.bAC);
+        equal = (Button)findViewById(R.id.bEqual);
+        plus = (Button)findViewById(R.id.bPlus);
+        minus = (Button)findViewById(R.id.bMinus);
+        multiple = (Button)findViewById(R.id.bMultiple);
+        divide = (Button)findViewById(R.id.bDivide);
+
+
+//
+//        one.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textView.append("1");
+//            }
+//        });
+//
+//        plus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textView.append("+");
+//            }
+//        });
+//
 
 
     }
 
-    public void onButtonCalACClicked(View v){
-        calculator.setPrice1(0);
-        calculator.setPrice2(0);
-        calculator.setResult(0);
-        textView.setText(0);
+    @Override
+    public void onClick(View v) {
+        int a = v.getId();
+        String sting = String.valueOf(a);
+        textView.setText(sting);
+
+        one.setOnClickListener();
     }
 
-    public void onButtonCal0Clicked(View v){
-
-    }
 
 
 
 
+
+
+
+
+    // TODO: 2020-04-04 리스너로 버튼 바꾸기.
+    // TODO: 2020-04-04 그런데.... 연산 부분은 지금처럼말고 string으로 받아서 split
+    // TODO: 2020-04-04 버튼 아이디 주기.
+    // TODO: 2020-04-04 버튼 초기화하기.
+    // TODO: 2020-04-04 리스너를 쓰는 방법은 두가지 - 1. 각각의 버튼에 주는거 2. 클래스 전체에 상속받아오기.
 
 
 }
